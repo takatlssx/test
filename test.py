9ffe5ea548cfc7acb4e429a28e2a90639c070ce1
takatlssx <69699607+takatlssx@users.noreply.github.com>
2020-08-31 09:30:18 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-    def copy_db(self,dest_db,dest_table_name,src_table_name):
+    def copy_db(self,src_db,src_table_name,dest_table_name):
+        if not os.path.exists(src_db):
+            self.error f+= ""
+            return False
+        try:
+            src_conn = sqlite.connection(src_db)
+            src_cursor = src_conn.cursor()
+            src_cursor.execute("select * from " + src_table_name)
+            data = src_cursor.fetchall()
         
