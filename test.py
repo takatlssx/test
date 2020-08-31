696171d339cde1c0953a4b8cecec2aff3355c3bc
takatlssx <69699607+takatlssx@users.noreply.github.com>
2020-08-31 13:02:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
     def clone_db(self,src_db,src_info,dest_db):
+        err = "データベースクローンエラー:<<DB.clone_db()\n"
+        if not os.path.exists(src_db):
+            self.error += err+f"{src_db}は存在しませんでした。\n"
+            return False
         
