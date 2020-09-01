06797d785443353ce79a54aab7002fe12d2f8e5d
takatlssx <69699607+takatlssx@users.noreply.github.com>
2020-09-01 12:11:23 +0900

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,7 +1,7 @@
     def import_csv(self,csv_path,table_name):
         err = "csvインポートエラー:<<import_csv()\n"
         if not os.path.exists(csv_path):
-            self.error += f"{err}csv:{csv_path}が見つかりませんでした。\n"
+            self.error += f"{err}csvファイル:{csv_path}が見つかりませんでした。\n"
             return False
         
         if not self.is_exist_table(table_name):
@@ -11,8 +11,7 @@
         data_2d = []
         try:
             with open(csv_path,"r",encoding="utf-8") as f:
-                for row in f:
-                    data_2d.append(row.split(','))
+                data_2d = [row.split(",") for row in f]
         except Exception as ex:
             self.error += f"{err}csvファイルをリストに変換できませんでした。\n{str(ex)}"
             return False
