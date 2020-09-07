19b2d43927840598402ccc63ae88000c1740d440
takatlssx <69699607+takatlssx@users.noreply.github.com>
2020-09-07 14:05:38 +0900

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,29 +1,13 @@
-    def import_csv(self,csv_path,table_name):
-        err = "csvインポートエラー:<<import_csv()\n"
-        if not os.path.exists(csv_path):
-            self.error += f"{err}csvファイル:{csv_path}が見つかりませんでした。\n"
-            return False
-        
-        if not self.is_exist_table(table_name):
-            self.error += f"{err}{table_name}テーブルは存在しません。\n"
-            return False
-
-        data_2d = []
-        try:
-            with open(csv_path,"r",encoding="utf-8") as f:
-                data_2d = [row.split(",") for row in f]
-        except Exception as ex:
-            self.error += f"{err}csvファイルをリストに変換できませんでした。\n{str(ex)}"
-            return False
-
-        if len(data_2d[0]) != len(self.info[table_name]["cols"]):
-            self.error += f"{err}csvファイルのデータ数と{table_name}テーブルのデータ数が一致しません。\n"
-            return False
-        
-        #data_2dの最初のデータは列名リストなので削除する
-        del data_2d[0]
-        if not self.insert_many(table_name,data_2d):
-            self.error += err
-            return False
-        
-        return True
+def get_data(self,table_name):
+    data = []
+    with open("","r",encoding="utf-8") as f:
+        is_start = False
+        for row in f:            
+            if "<-@" == row:
+                is_start = False
+            if is_start:
+                data.append(row.split(","))
+            if "@->" in row:
+                if row.replace("@->","") == table_name:
+                    is_start = True
+    retrun data
